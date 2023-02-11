12227cb380e05f3296a7c339d09163d4ea4ecd4f
Tamar Entin <tamarentin13@gmail.com>
2023-02-11 14:14:55 +0200

    travel advisor api

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_KEY = "YOUR_API_KEY";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-key": API_KEY,
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
